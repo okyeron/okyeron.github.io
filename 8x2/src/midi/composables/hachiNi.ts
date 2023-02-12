d9a51ab890a5f588b3568e7c9784e0d7d25e87a7
Nicholas Sidwell <nicholas@sidwell.dev>
2023-02-11 20:02:33 -0800

    Fix formatting

--- 8x2/src/midi/composables/hachiNi.ts
+++ 8x2/src/midi/composables/hachiNi.ts
@@ -15,7 +15,7 @@ const mappings = reactive<Mappings>({});
 
 const midiBank = ref<Bank>(1);
 
-const potentiometers = ref<number[]>([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]);
+const potentiometers = ref<number[]>([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
 
 // External to this module, we expose a writeable computed where accessing
 // the value proxies to midiBank's value, but setting the value merely
@@ -128,19 +128,19 @@ function getPort(direction: Port): Input | Output | null {
 const inputListenerCallbacks: InputListenerCallbacks = {
   // TODO: Figure out how to receive knob states
   controlchange: [
-      (e) => {
-        if (e.message.statusByte === 176) {
-          // console.debug(e);
-          const [control, controlValue] = e.message.dataBytes;
-          // console.debug(`channel ${e.message.channel}. cntrl: ${control}:${controlValue}`);
-          const thisPot = mappings[midiBank.value]?.usb.ccs;
-          const thisIdx = thisPot?.findIndex((el) => el === control);
-          console.debug(thisIdx);
-          if (thisIdx != null) {
-            potentiometers.value[thisIdx] = controlValue;
-          }        
+    (e) => {
+      if (e.message.statusByte === 176) {
+        // console.debug(e);
+        const [control, controlValue] = e.message.dataBytes;
+        // console.debug(`channel ${e.message.channel}. cntrl: ${control}:${controlValue}`);
+        const thisPot = mappings[midiBank.value]?.usb.ccs;
+        const thisIdx = thisPot?.findIndex((el) => el === control);
+        console.debug(thisIdx);
+        if (thisIdx != null) {
+          potentiometers.value[thisIdx] = controlValue;
         }
-      },
+      }
+    },
   ],
   sysex: [
     (e) => {
