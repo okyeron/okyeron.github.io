62bf1edbeb765d9c1f29e6b5ff782e67d4147dbc
Nicholas Sidwell <nicholas@sidwell.dev>
2023-02-18 15:09:01 -0800

    Fix NumberInput render bug

--- 8x2/src/components/NumberInput.vue
+++ 8x2/src/components/NumberInput.vue
@@ -58,26 +58,26 @@ const onChange = (e: Event) => {
     } else {
       current.value = parsedValue;
     }
-
-    /* If the value the input is bound to hasn't changed as a result of this validation logic,
-     * then the component won't be re-rendered. This leaves the possibility that the HTML input
-     * element's internal value could diverge from the model value.
-     *
-     * An example:
-     *
-     * The input has a max value of 16, and the current model value is 16. The user's cursor
-     * is in the input after the 6 and hits '2'. Now, the input has a value of 162. This
-     * validation logic is triggered, finds that the value is greater than the max, and sets
-     * the model value to the max i.e. 16. Effectively, the value hasn't changed, and so the
-     * input does not re-render. We could have some counter that gets incremented every time
-     * the validation runs, and bind the input's key to the counter. This would force a
-     * re-render on validation; however, it is not a great user experience as it would cause
-     * the input to lose focus.
-     *
-     * This is a fix to re-sync the input's value out-of-band of Vue's reactivity system.
-     */
-    (e.target as HTMLInputElement).value = current.value.toString();
   }
+
+  /* If the value the input is bound to hasn't changed as a result of this validation logic,
+   * then the component won't be re-rendered. This leaves the possibility that the HTML input
+   * element's internal value could diverge from the model value.
+   *
+   * An example:
+   *
+   * The input has a max value of 16, and the current model value is 16. The user's cursor
+   * is in the input after the 6 and hits '2'. Now, the input has a value of 162. This
+   * validation logic is triggered, finds that the value is greater than the max, and sets
+   * the model value to the max i.e. 16. Effectively, the value hasn't changed, and so the
+   * input does not re-render. We could have some counter that gets incremented every time
+   * the validation runs, and bind the input's key to the counter. This would force a
+   * re-render on validation; however, it is not a great user experience as it would cause
+   * the input to lose focus.
+   *
+   * This is a fix to re-sync the input's value out-of-band of Vue's reactivity system.
+   */
+  (e.target as HTMLInputElement).value = current.value.toString();
 };
 
 const onKeyDown = (e: KeyboardEvent) => {
