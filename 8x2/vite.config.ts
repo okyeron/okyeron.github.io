622f101237c37d79aa92331721d349a682e8bd76
Nicholas Sidwell <nicholas@sidwell.dev>
2023-02-22 22:22:24 -0800

    Add base public path to 8x2 app

--- 8x2/vite.config.ts
+++ 8x2/vite.config.ts
@@ -4,6 +4,7 @@ import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/okyeron.github.io/',
   plugins: [vue()],
   resolve: {
     alias: [{ find: '@', replacement: resolve(__dirname, './src') }],
