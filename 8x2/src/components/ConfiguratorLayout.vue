d61425faf2d4a273fc95d1587dd1d5f835142da9
Nicholas Sidwell <nicholas@sidwell.dev>
2023-03-05 13:17:48 -0800

    Add modal overlay

---
+++ 8x2/src/components/ConfiguratorLayout.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="layout">
+    <div class="configurator">
+      <slot name="configurator" />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+.layout {
+  align-items: stretch;
+  display: grid;
+  grid-template-areas:
+    '.'
+    'configurator'
+    '.';
+
+  grid-template-rows: 1fr auto 1fr;
+  height: 100%;
+}
+
+.layout .configurator {
+  grid-area: configurator;
+  margin: 0 auto;
+  max-width: 1280px;
+}
+</style>
