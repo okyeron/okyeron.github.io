7db4327899daf1c943c1845688989b7510fd6907
Nicholas Sidwell <nicholas@sidwell.dev>
2023-03-07 19:30:05 -0800

    Add modal confirmation functionality

--- 8x2/src/components/ModalOverlay.vue
+++ 8x2/src/components/ModalOverlay.vue
@@ -1,11 +1,21 @@
 <template>
   <Teleport to="body">
     <Transition>
-      <section v-show="showModal" @click="closeModal" class="overlay">
+      <section v-show="showModal" @click="close(false)" :class="['overlay', { cover: modalConfig.cover }]">
         <div v-show="showModal" @click.stop class="modal">
-          <component :is="content" />
+          <div v-if="typeof content === 'string'" class="string-content">
+            {{ content }}
+          </div>
 
-          <button @click="closeModal" class="close-button">x</button>
+          <component v-else :is="content" />
+
+          <div v-show="modalConfig.confirm" class="row align-center justify-end actions">
+            <button @click="close(true)" class="okay">Okay</button>
+
+            <button @click="close(false)">Cancel</button>
+          </div>
+
+          <button @click="close(false)" class="close-button">x</button>
         </div>
       </section>
     </Transition>
@@ -13,17 +23,28 @@
 </template>
 
 <script lang="ts" setup>
-import { useModal } from '@/composables/modal';
 import { computed } from 'vue';
+import { useModalContainer } from '@/composables/modal';
 
-const { content, setModalContent } = useModal();
+const { closeModal, content, modalConfig } = useModalContainer();
 
 const showModal = computed(() => content.value != null);
 
-const closeModal = () => setModalContent(null);
+const close = (confirmState = false) => {
+  closeModal(confirmState);
+};
 </script>
 
 <style scoped>
+.actions {
+  gap: 1ch;
+  margin-top: 0.5em;
+}
+
+.okay {
+  border: none;
+}
+
 .close-button {
   background: none;
   border: none;
@@ -36,25 +57,48 @@ const closeModal = () => setModalContent(null);
   vertical-align: middle;
   width: 3ch;
 }
+
+.close-button:hover {
+  background-color: var(--background-color);
+  border: 1px solid white;
+  color: white;
+}
+
 .modal {
   background-color: var(--background-color);
   border: 1px dashed var(--border-color);
   border-radius: 1ch;
   padding: 1em;
   position: relative;
+  width: min(600px, 100%);
 }
+
+.modal:has(.string-content) {
+  width: min(300px, 100%);
+}
+
 .overlay {
   align-items: center;
-  background: rgba(0, 0, 0, 0.52);
-  backdrop-filter: blur(2px);
   bottom: 0;
   display: flex;
   left: 0;
   justify-content: center;
-  position: absolute;
+  overflow: scroll;
+  overscroll-behavior: contain;
+  padding: 1em;
+  position: fixed;
   right: 0;
   top: 0;
-  z-index: 2;
+  z-index: 3;
+}
+
+.overlay.cover {
+  background: rgba(0, 0, 0, 0.52);
+  backdrop-filter: blur(2px);
+}
+
+.string-content {
+  text-align: center;
 }
 
 .v-enter-active,
